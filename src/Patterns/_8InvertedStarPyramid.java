34566acc70478d21970e241c1f5c2e09912976aa
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-04-23 23:48:19 +0530

    Patterns | Basic Maths
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

---
+++ src/Patterns/_8InvertedStarPyramid.java
@@ -0,0 +1,27 @@
+package Patterns;
+
+import java.util.Scanner;
+
+//    * * * * * * * * *
+//      * * * * * * *
+//        * * * * *
+//          * * *
+//            *
+public class _8InvertedStarPyramid {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int row = n; row >= 1 ; row--) {
+            int space = 1;
+            while(space <= n - row){
+                System.out.print("  ");
+                space = space + 1;
+            }
+            for (int col = 1; col <= (2*row - 1) ; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Patterns/_9StarDiamondPattern.java
@@ -0,0 +1,37 @@
+package Patterns;
+
+import java.util.Scanner;
+
+//            *
+//          * * *
+//        * * * * *
+//      * * * * * * *
+//        * * * * *
+//          * * *
+//            *
+public class _9StarDiamondPattern {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int k = 1;
+        int row = 1;
+        while(k <= 2*n-1) {
+            int space = 1;
+            while(space <= n - row){
+                System.out.print("  ");
+                space = space + 1;
+            }
+            for (int col = 1; col <= (2*row - 1) ; col++) {
+                System.out.print("* ");
+            }
+            if(k<n) {
+                row++;
+            } else {
+                row--;
+            }
+            k++;
+            System.out.println();
+        }
+    }
+}
